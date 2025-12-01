819c942556052dc3970bf7b42b44d6f9928bfa24
DrouicheMouad <lycinal100622010843400@gmail.com>
2025-12-01 22:46:16 +0100

    add exo 8

---
+++ Exo 8/Exo 8/C Projec Template.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	printf("Enter a "); scanf("%d", &a);
+	printf("Enter b "); scanf("%d", &b);
+	if (a < b) {
+		printf("\nQuotient = 0");
+		printf("\nRest = %d", a);
+	}
+	else {
+		int q = 1;
+		int r = 0;
+		while (a - b*q >= b) {
+			q++;
+		}
+		r = a - b*q;
+		printf("%d / %d = %d.%d + %d", a, b, q, b, r);
+		printf("\nQuotient = %d", q);
+		printf("\nRest = %d", r);
+
+
+	}
+
+
+
+	return 0;
+}
