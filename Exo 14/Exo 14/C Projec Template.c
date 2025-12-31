2cbc8a3b600351e757ce8e67a478db37909da3ed
DrouicheMouad <lycinal100622010843400@gmail.com>
2025-12-31 12:18:02 +0100

    Update C Projec Template.c

--- Exo 14/Exo 14/C Projec Template.c
+++ Exo 14/Exo 14/C Projec Template.c
@@ -2,25 +2,24 @@
 
 int main() {
 	int Height;
-	printf("Enter the hieght "); 
+	printf("Enter the height ");
 	scanf("%d", &Height);
 	for (int i = 1; i <= Height; i++) {
-		for (int j = 1; j <= i; j++) {
-			if (i <= 2) {
+		if (i <= 2) {
+			for (int j = 1; j <= i; j++)
 				printf("*");
-			}
-			else if(i<Height) {
-				printf("*");
-				for (j = 2; j < i; j++) {
-					printf(" ");
-				}
-				printf("*");
-			}
-			else
-				for (j = 1; j <= Height; j++) {
-					printf("*");
-				}
 		}
+		else if (i < Height) {
+			printf("*");
+			for (int j = 2; j < i; j++) {
+				printf(" ");
+			}
+			printf("*");
+		}
+		else
+			for (int j = 1; j <= Height; j++) {
+				printf("*");
+			}
 		printf("\n");
 	}
 	return 0;
